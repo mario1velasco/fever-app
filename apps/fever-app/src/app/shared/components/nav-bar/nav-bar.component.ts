e0d85fbb502de9771f450d204996313547d3ff1b
mariovelascoalonso <mario.velasco@bizaway.com>
2024-08-30 22:35:59 +0200

    feat(app): Create navbar

---
+++ apps/fever-app/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -0,0 +1,22 @@
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '@fever-pets/ui';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [CommonModule, ButtonComponent],
+  templateUrl: './nav-bar.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class NavBarComponent {
+  // **********************
+  // ****** Events  *******
+  // **********************
+  /**
+   * Handles the click event of the view details button.
+   */
+  public onClickBtnContact() {
+    window.location.href = 'https://www.linkedin.com/in/mariovelascoalonso/';
+  }
+}
