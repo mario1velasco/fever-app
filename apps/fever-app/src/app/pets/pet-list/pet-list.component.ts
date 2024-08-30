9722722f21b39f4f808ee67532561c7196a0dd01
mariovelascoalonso <mario.velasco@bizaway.com>
2024-08-30 22:39:20 +0200

    feat(app): PetListComponent style and working

--- apps/fever-app/src/app/pets/pet-list/pet-list.component.ts
+++ apps/fever-app/src/app/pets/pet-list/pet-list.component.ts
@@ -2,11 +2,13 @@ import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { toSignal } from '@angular/core/rxjs-interop';
 import { PetsService } from '../shared/pets.service';
+import { ButtonComponent } from '@fever-pets/ui';
+import { NavBarComponent } from '../../shared/components/nav-bar/nav-bar.component';
 
 @Component({
   selector: 'app-pet-list',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent, NavBarComponent],
   providers: [PetsService],
   templateUrl: './pet-list.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -16,4 +18,16 @@ export class PetListComponent {
   public petService = inject(PetsService);
   // * Signals
   public pets = toSignal(this.petService.getList());
+
+  // **********************
+  // ****** Events  *******
+  // **********************
+  /**
+   * Handles the click event of the view details button.
+   *
+   * @param {number} id - The ID of the item to view details for.
+   */
+  public onClickBtnViewDetails(id: number) {
+    console.log('View Details', id);
+  }
 }
