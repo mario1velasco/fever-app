7dd0a308e1798a304398d5df3ded28e4e9bd56e1
mariovelascoalonso <mario.velasco@bizaway.com>
2024-08-31 17:33:40 +0200

    style(app): Create FooterComponent and change title and icon

--- apps/fever-app/src/app/app.component.ts
+++ apps/fever-app/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { NavBarComponent } from './shared/components/nav-bar/nav-bar.component';
+import { FooterComponent } from './shared/components/footer/footer.component';
 
 @Component({
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NavBarComponent, FooterComponent],
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
