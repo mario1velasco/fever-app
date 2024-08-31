48e2cd125acf8348d3ea0037446b2255d56f7fd2
mariovelascoalonso <mario.velasco@bizaway.com>
2024-08-31 17:36:13 +0200

    feat(app): Finish PetDetailsComponent. adding data to display and styled

--- apps/fever-app/src/app/pets/pet-details/pet-details.component.ts
+++ apps/fever-app/src/app/pets/pet-details/pet-details.component.ts
@@ -1,24 +1,45 @@
 import {
   ChangeDetectionStrategy,
   Component,
+  computed,
   inject,
-  signal,
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PetsService } from '../shared/pets.service';
 import { toSignal } from '@angular/core/rxjs-interop';
-import { Pet } from '../shared/pets.types';
+import { ActivatedRoute, Router } from '@angular/router';
+import { ButtonComponent } from '@fever-pets/ui';
 
 @Component({
   selector: 'app-pet-details',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ButtonComponent],
   templateUrl: './pet-details.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class PetDetailsComponent {
   // * Injectors
+  public activatedRoute = inject(ActivatedRoute);
   public petService = inject(PetsService);
+  public router = inject(Router);
+  // * Routing Variables
+  public userId = this.activatedRoute.snapshot.paramMap.get('petId');
   // * Signals
-  public pets = toSignal(this.petService.getList());
+  public pet = toSignal(this.petService.get(this.userId));
+  // * Variables
+  public petHealth = computed(() => {
+    const pet = this.pet();
+    if (!pet) return 0;
+    return this.petService.calculatePetHealth(pet);
+  });
+
+  // **********************
+  // ****** Events  *******
+  // **********************
+  /**
+   * Handles the click event of the view details button.
+   */
+  public onClickBtnBack() {
+    this.router.navigate(['']);
+  }
 }
