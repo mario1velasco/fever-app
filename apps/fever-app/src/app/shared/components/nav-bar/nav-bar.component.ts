0386d5b2ffb0c51cc7df27b29ad3360a03b35b03
mariovelascoalonso <mario.velasco@bizaway.com>
2024-09-01 09:23:42 +0200

    feat(app): Implement PetOfTheDay into the NavBar
    Create a Dialog to show pet of the day will be show when we click on the petOfDay button

--- apps/fever-app/src/app/shared/components/nav-bar/nav-bar.component.ts
+++ apps/fever-app/src/app/shared/components/nav-bar/nav-bar.component.ts
@@ -1,22 +1,54 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  DestroyRef,
+  inject,
+  signal,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ButtonComponent } from '@fever-pets/ui';
+import { ButtonComponent, DialogComponent } from '@fever-pets/ui';
+import { Router } from '@angular/router';
+import { PetsService } from '../../../pages/pets/shared/pets.service';
+import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
+import { PetOfTheDay } from '../../../pages/pets/shared/pets.types';
 
 @Component({
   selector: 'app-nav-bar',
   standalone: true,
-  imports: [CommonModule, ButtonComponent],
+  imports: [CommonModule, ButtonComponent, DialogComponent],
   templateUrl: './nav-bar.component.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class NavBarComponent {
+  // * Injectors
+  public router = inject(Router);
+  public petService = inject(PetsService);
+  private destroyRef = inject(DestroyRef);
+  // * Variables
+  public isOpen = signal<boolean>(false);
+  public petOfTheDay: PetOfTheDay | undefined;
+
   // **********************
   // ****** Events  *******
   // **********************
   /**
    * Handles the click event of the view details button.
    */
-  public onClickBtnContact() {
+  onClickBtnContact() {
     window.open('https://www.linkedin.com/in/mariovelascoalonso/', '_blank');
+  }
+
+  onClickBtnPetOfTheDay() {
+    this.petService
+      .getPetOfTheDay()
+      .pipe(takeUntilDestroyed(this.destroyRef))
+      .subscribe((petOfTheDay) => {
+        this.petOfTheDay = petOfTheDay;
+        this.isOpen.set(true);
+      });
+  }
+
+  onClickDialogConfirmBtn() {
+    this.isOpen.set(false);
   }
 }
