23a8d63e8835c83ba61dd465de9e932ce0af4ecd
mariovelascoalonso <mario.velasco@bizaway.com>
2024-09-02 07:02:11 +0200

    test(app): Enabled experimentalStudio on cypress

--- apps/fever-app-e2e/cypress.config.ts
+++ apps/fever-app-e2e/cypress.config.ts
@@ -2,6 +2,15 @@ import { nxE2EPreset } from '@nx/cypress/plugins/cypress-preset';
 
 import { defineConfig } from 'cypress';
 
+const cypressJsonConfig = {
+  // chromeWebSecurity: false,
+  experimentalStudio: true,
+  screenshotsFolder: '../../dist/cypress/apps/fever-app-e2e/screenshots',
+  video: true,
+  videosFolder: '../../dist/cypress/apps/fever-app-e2e/videos',
+  viewportHeight: 1200,
+  viewportWidth: 1600,
+};
 export default defineConfig({
   e2e: {
     ...nxE2EPreset(__filename, {
@@ -13,6 +22,7 @@ export default defineConfig({
       ciWebServerCommand: 'npx nx run fever-app:serve-static',
       ciBaseUrl: 'http://localhost:4200',
     }),
+    ...cypressJsonConfig,
     baseUrl: 'http://localhost:4200',
   },
 });
